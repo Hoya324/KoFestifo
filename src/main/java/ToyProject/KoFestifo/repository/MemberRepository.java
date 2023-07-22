5255df762d6ef275457800643773163715da26ab
Hoya324 <gangho324@naver.com>
2023-07-22 18:23:52 +0900

    feat: MemberRepository 생성
    회원 저장 객체
    저장, id, name으로 조회, 전체 조회 구현

--- src/main/java/ToyProject/KoFestifo/repository/MemberRepository.java
+++ src/main/java/ToyProject/KoFestifo/repository/MemberRepository.java
@@ -1,12 +1,34 @@
 package ToyProject.KoFestifo.repository;
 
+import ToyProject.KoFestifo.domain.Member;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findByName(String username) {
+        return em.createQuery("SELECT m FROM Member m "
+            + "where m.username = :username", Member.class)
+            .setParameter("username", username)
+            .getResultList();
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("SELECT m FROM Member m", Member.class)
+            .getResultList();
+    }
 }
