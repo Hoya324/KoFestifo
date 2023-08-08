cc7171601a9e1ee1cb9a2b629ea0677f39aa185f
나경호 <gangho324@gmail.com>
2023-08-08 17:52:21 +0900

    refactor: 메서드명 확장성 높도록 변경

--- src/main/java/ToyProject/KoFestifo/repository/MemberRepository.java
+++ src/main/java/ToyProject/KoFestifo/repository/MemberRepository.java
@@ -1,34 +1,36 @@
 package ToyProject.KoFestifo.repository;
 
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
 import ToyProject.KoFestifo.domain.Member;
 import jakarta.persistence.EntityManager;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
 public class MemberRepository {
 
-    private final EntityManager em;
+	private final EntityManager em;
 
-    public void save(Member member) {
-        em.persist(member);
-    }
+	public void save(Member member) {
+		em.persist(member);
+	}
 
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
+	public Member findOne(Long id) {
+		return em.find(Member.class, id);
+	}
 
-    public List<Member> findByName(String username) {
-        return em.createQuery("SELECT m FROM Member m "
-            + "where m.username = :username", Member.class)
-            .setParameter("username", username)
-            .getResultList();
-    }
+	public List<Member> findByElement(String username) {
+		return em.createQuery("SELECT m FROM Member m "
+				+ "where m.username = :username", Member.class)
+			.setParameter("username", username)
+			.getResultList();
+	}
 
-    public List<Member> findAll() {
-        return em.createQuery("SELECT m FROM Member m", Member.class)
-            .getResultList();
-    }
+	public List<Member> findAll() {
+		return em.createQuery("SELECT m FROM Member m", Member.class)
+			.getResultList();
+	}
 }
