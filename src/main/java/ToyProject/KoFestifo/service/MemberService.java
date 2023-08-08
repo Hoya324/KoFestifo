837cf540dcc931968e65babea7973b836b0995c8
나경호 <gangho324@gmail.com>
2023-08-08 17:52:54 +0900

    refactor: Transactional 기본 옵션 readOnly로 변경

--- src/main/java/ToyProject/KoFestifo/service/MemberService.java
+++ src/main/java/ToyProject/KoFestifo/service/MemberService.java
@@ -1,43 +1,46 @@
 package ToyProject.KoFestifo.service;
 
-import ToyProject.KoFestifo.domain.Member;
-import ToyProject.KoFestifo.repository.MemberRepository;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import ToyProject.KoFestifo.domain.Member;
+import ToyProject.KoFestifo.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements KofestifoService<Member> {
 
-  private final MemberRepository memberRepository;
+	private final MemberRepository memberRepository;
 
-  @Transactional
-  public Long join(Member member) {
-    validate(member);
-    memberRepository.save(member);
-    return member.getId();
-  }
+	@Override
+	@Transactional
+	public Long join(Member member) {
+		validate(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
 
-  public List<Member> findMembers() {
-    return memberRepository.findAll();
-  }
+	@Override
+	public List<Member> findMembers() {
+		return memberRepository.findAll();
+	}
 
-  private void validate(Member member) {
-    validateDuplicateMember(member);
-  }
+	private void validate(Member member) {
+		validateDuplicateMember(member);
+	}
 
-  private void validateDuplicateMember(Member member) {
-    List<Member> findMembers = memberRepository.findByName(member.getUsername());
-    if (isDuplicated(findMembers)) {
-      throw new IllegalStateException("[Error] 이미 존재하는 회원입니다.");
-    }
-  }
+	private void validateDuplicateMember(Member member) {
+		List<Member> findMembers = memberRepository.findByElement(member.getUsername());
+		if (isDuplicated(findMembers)) {
+			throw new IllegalStateException("[Error] 이미 존재하는 회원입니다.");
+		}
+	}
 
-  private static boolean isDuplicated(List<Member> findMembers) {
-    return !findMembers.isEmpty();
-  }
+	private static boolean isDuplicated(List<Member> findMembers) {
+		return !findMembers.isEmpty();
+	}
 
 }
