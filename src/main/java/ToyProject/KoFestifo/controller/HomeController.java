437a10d591c669912885400e2cc4c9644c97cef5
나경호 <gangho324@gmail.com>
2023-08-08 17:47:39 +0900

    refactor: HomeController에서 json 바로 받도록 변경

--- src/main/java/ToyProject/KoFestifo/controller/HomeController.java
+++ src/main/java/ToyProject/KoFestifo/controller/HomeController.java
@@ -1,13 +1,31 @@
 package ToyProject.KoFestifo.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
+import ToyProject.KoFestifo.domain.Event;
+import ToyProject.KoFestifo.service.KofestifoService;
+import ToyProject.KoFestifo.util.SeoulOpenAPIManager;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
+@Controller
+@Slf4j
+@RequiredArgsConstructor
+public class HomeController {
+	private final KofestifoService<Event> eventService;
+	private final SeoulOpenAPIManager seoulOpenAPIManager;
+
+	@GetMapping("/")
+	public String index(Model model) throws IOException, ParseException {
+		seoulOpenAPIManager.readJson();
+		List<Event> events = eventService.findMembers();
+		model.addAttribute("events", events);
+		return "home";
+	}
 }
