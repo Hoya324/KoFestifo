c8bcaca5bedf7bd11c0007abe42b091cc58a52b8
나경호 <gangho324@gmail.com>
2023-08-08 17:45:17 +0900

    feat: EventDTO 생성

---
+++ src/main/java/ToyProject/KoFestifo/dto/EventDTO.java
@@ -0,0 +1,19 @@
+package ToyProject.KoFestifo.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class EventDTO {
+
+	private String main_img;
+	private String org_link;
+	private String title;
+	private String place;
+	private String use_fee;
+	private String date;
+	private String codename;
+}
