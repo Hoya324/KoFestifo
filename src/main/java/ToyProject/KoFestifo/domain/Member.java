68d662256c3c21b993930b125aa0558709c0b8f7
나경호 <gangho324@gmail.com>
2023-08-08 17:49:39 +0900

    refactor: member_id 옵션 추가

--- src/main/java/ToyProject/KoFestifo/domain/Member.java
+++ src/main/java/ToyProject/KoFestifo/domain/Member.java
@@ -1,39 +1,43 @@
 package ToyProject.KoFestifo.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
-
-import java.util.ArrayList;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Getter
+@Setter
 @RequiredArgsConstructor
 public class Member {
 
-    @Id @GeneratedValue
-    @Column(name = "member_id")
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "member_id", nullable = false, unique = true)
+	private Long id;
 
-    @Column(unique = true)
-    private String username;
+	@Column(unique = true)
+	private String username;
 
-    private String password;
+	private String password;
 
-    @Column(unique = true)
-    private String email;
+	@Column(unique = true)
+	private String email;
 
-    @OneToMany(mappedBy = "member")
-    private final List<Event> events = new ArrayList<>();
+	@OneToMany(mappedBy = "member")
+	private final List<RegisteredEvent> events = new ArrayList<>();
 
-    public Member(String username, String password, String email) {
-        this.username = username;
-        this.password = password;
-        this.email = email;
-    }
+	public Member(String username, String password, String email) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
 }
