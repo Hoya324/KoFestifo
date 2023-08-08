1d770247674260ee86efbd4379a3314b9586aa51
나경호 <gangho324@gmail.com>
2023-08-08 17:51:27 +0900

    refactor: RequestMapping으로 초기 경로 설정

--- src/main/java/ToyProject/KoFestifo/controller/MemberController.java
+++ src/main/java/ToyProject/KoFestifo/controller/MemberController.java
@@ -1,43 +1,48 @@
 package ToyProject.KoFestifo.controller;
 
-import ToyProject.KoFestifo.domain.Member;
-import ToyProject.KoFestifo.service.MemberService;
-import jakarta.validation.Valid;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ToyProject.KoFestifo.domain.Member;
+import ToyProject.KoFestifo.dto.MemberDTO;
+import ToyProject.KoFestifo.service.KofestifoService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
-  private final MemberService memberService;
+	private final KofestifoService<Member> memberService;
 
-  @GetMapping("/members/new")
-  public String createForm(Model model) {
-    model.addAttribute("memberForm", new MemberForm());
-    return "members/createMemberForm";
-  }
+	@GetMapping("/new")
+	public String createForm(Model model) {
+		model.addAttribute("memberDTO", new MemberDTO());
+		return "members/createMemberForm";
+	}
 
-  @PostMapping("/members/new")
-  public String create(@Valid MemberForm form, BindingResult result) {
+	@PostMapping("/new")
+	public String create(@Valid MemberDTO form, BindingResult result) {
 
-    if (result.hasErrors()) {
-      return "members/createMemberForm";
-    }
-    Member member = new Member(form.getUsername(), form.getPassword(), form.getEmail());
-    memberService.join(member);
-    return "redirect:/";
-  }
+		if (result.hasErrors()) {
+			return "members/createMemberForm";
+		}
+		Member member = new Member(form.getUsername(), form.getPassword(), form.getEmail());
+		memberService.join(member);
+		return "redirect:/";
+	}
 
-  @GetMapping("/members")
-  public String showList(Model model) {
-    List<Member> members = memberService.findMembers();
-    model.addAttribute("members", members);
-    return "members/memberList";
-  }
+	@GetMapping
+	public String showList(Model model) {
+		List<Member> members = memberService.findMembers();
+		model.addAttribute("members", members);
+		return "members/memberList";
+	}
 }
